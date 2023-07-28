398b1d8bcef9071ff491c1fbb3454b4bfa6d569c
“Musandiwa” <musasinyoc@gmail.com>
2023-07-28 12:37:19 -0700

    Printf Project

--- main.h
+++ main.h
@@ -59,6 +59,12 @@ int (*get_print(char s))(va_list, flags_t *);
 /* get_flag */
 int get_flag(char s, flags_t *f);
 
+/* get_precision */
+int get_precision(const char *format, int *i, va_list list)
+
+/* get_width */
+int get_width(const char *format, int *i, va_list list)
+
 /* print_alpha */
 int print_string(va_list list);
 int print_char(va_list list);
