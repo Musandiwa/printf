4d919f44e34fdec745812870839aedd6c1bdf979
“Musandiwa” <musasinyoc@gmail.com>
2023-07-28 13:20:19 -0700

    Printf Project

--- print_alpha.c
+++ print_alpha.c
@@ -3,12 +3,16 @@
 /**
  * print_string - loops through a string and prints
  * every character
- * @list: va_list arguments from _printf
+ * @l: va_list arguments from _printf
+ * @f: pointer to the struct flags that determines
+ * if a flag is passed to _printf
  * Return: number of char printed
  */
-int print_string(va_list list)
+int print_string(va_list l, flags_t *f)
 {
-	char *s = va_arg(list, char *);
+	char *s = va_arg(l, char *);
+
+	(void)f;
 
 	if (!s)
 		s = "(null)";
@@ -17,11 +21,14 @@ int print_string(va_list list)
 
 /**
  * print_char - prints a character
- * @list: va_list arguments from _printf
+ * @l: va_list arguments from _printf
+ * @f: pointer to the struct flags that determines
+ * if a flag is passed to _printf
  * Return: number of char printed
  */
-int print_char(va_list list)
+int print_char(va_list l, flags_t *f)
 {
+	(void)f;
 	_putchar(va_arg(l, int));
 	return (1);
 }
--- print_nums.c
+++ print_nums.c
@@ -2,13 +2,14 @@
 
 /**
  * print_int - prints an integer
- * @list: va_list of arguments from _printf
+ * @l: va_list of arguments from _printf
  * @f: pointer to the struct flags determining
+ * if a flag is passed to _printf
  * Return: number of char printed
  */
-int print_int(va_list list, flags_t *f)
+int print_int(va_list l, flags_t *f)
 {
-	int n = va_arg(list, int);
+	int n = va_arg(l, int);
 	int res = count_digit(n);
 
 	if (f->space == 1 && f->plus == 0 && n >= 0)
@@ -23,14 +24,17 @@ int print_int(va_list list, flags_t *f)
 
 /**
  * print_unsigned - prints an unsigned integer
- * @list: va_list of arguments from _printf
+ * @l: va_list of arguments from _printf
+ * @f: pointer to the struct flags determining
+ * if a flag is passed to _printf
  * Return: number of char printed
  */
-int print_unsigned(va_list list)
+int print_unsigned(va_list l, flags_t *f)
 {
 	unsigned int u = va_arg(l, unsigned int);
 	char *str = convert(u, 10, 0);
 
+	(void)f;
 	return (_puts(str));
 }
 
